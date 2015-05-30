f98d918725a4f2e49e32dd2b66821760470eca13
Katsiaryna Sheleh <kate.sheleh@gmail.com>
2015-05-30 18:40:50 +0200

    коррекция отступов в файле

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,3 @@
-//counter
-
-// var pluralizeWord = function(i, str1, str2, str3) {
-//       var plural = function(a) {
-//         if (a % 10 === 1 && a % 100 !== 11) {
-//           return 0;
-//         } else if (a % 10 >= 2 && a % 10 <= 4 && (a % 100 < 10 || a % 100 >= 20)) {
-//           return 1;
-//         } else {
-//           return 2;
-//         }
-//       };
-//       switch (plural(i)) {
-//         case 0:
-//           return str1;
-//         case 1:
-//           return str2;
-//         default:
-//           return str3;
-//       }
-//     };
-
-// var count = 1,
-//   countEl = document.getElementById("duration-day");
-
-// function plus() {
-//    count++;
-//    countEl.value = count + ' ' + pluralizeWord(count, 'день', 'дня', 'дней');
-// }
-
-// function minus() {
-//   if (count > 1) {
-//     count--;
-//     countEl.value = count + ' ' + pluralizeWord(count, 'день', 'дня', 'дней');
-//   }
-// }
-
-//   var counter = 1;
-//     var countElem = document.getElementById("traveler-number");
-//     function plusPerson(){
-//         counter++;
-//         countElem.value = counter + " чел";
-//     }
-//     function minusPerson(){
-//       if (counter > 1) {
-//         counter--;
-//         countElem.value = counter + " чел";
-//       }
-//     }
-
-
 /*============================
 =            MENU            =
 ============================*/
@@ -106,7 +55,7 @@ menuControl.addEventListener("click", function(ev){
   };
 
 
-   for (var i = 0; i < plusBtnsLength; i++) {
+  for (var i = 0; i < plusBtnsLength; i++) {
     plusBtns[i].addEventListener('click', onPlusClicked);
   };
 
@@ -231,10 +180,10 @@ menuControl.addEventListener("click", function(ev){
 
 //FormData
 (function() {
- var form = document.querySelector(".form");
- var area = form.querySelector(".pic-list");
- var template = document.querySelector("#image-template").innerHTML;
- var queue = [];
+  var form = document.querySelector(".form");
+  var area = form.querySelector(".pic-list");
+  var template = document.querySelector("#image-template").innerHTML;
+  var queue = [];
 
 
 
@@ -244,7 +193,7 @@ form.querySelector("#js-upload-photo").addEventListener("change", function() {
   var files = this.files;
   for (var i = 0; i < files.length; i++) {
   preview(files[i]);
- }
+}
 });
 
 
@@ -298,13 +247,13 @@ function removePreview(li) {
 
 
 
- form.addEventListener("submit", function(event) {
-   event.preventDefault();
-   var data = new FormData(form);
-     request(data, function(response) {
-     console.log(response);
-     });
- });
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  var data = new FormData(form);
+    request(data, function(response) {
+    console.log(response);
+  });
+});
 
 
 
@@ -314,13 +263,13 @@ function request(data, fn) {
   var xhr = new XMLHttpRequest();
   var time = (new Date()).getTime();
   xhr.open("post", "http://simonenko.su/academy/echo?" + time);
-     xhr.addEventListener("readystatechange", function() {
-     if (xhr.readyState == 4) {
-     fn(xhr.responseText);
-     }
-     });
-   xhr.send(data);
- }
+    xhr.addEventListener("readystatechange", function() {
+      if (xhr.readyState == 4) {
+      fn(xhr.responseText);
+    }
+    });
+  xhr.send(data);
+}
 })();
 
 
